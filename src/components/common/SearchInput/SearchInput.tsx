0d4624099fbf85c15bc14f48a1d287369c3c141b
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-14 22:11:44 +0100

    Create a Search component

---
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -0,0 +1,33 @@
+import { InputHTMLAttributes } from "react";
+
+import Icon from "~/components/common/Icon";
+
+interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  onClick?: () => void;
+}
+
+const SearchInput = ({ onClick, ...inputProps }: SearchInputProps) => {
+  return (
+    <div className="mt-2 w-full sm:col-span-2">
+      <div className="flex w-full rounded-md shadow-sm">
+        <input
+          {...inputProps}
+          type="text"
+          name="Tracking number"
+          id="trackingNumber"
+          placeholder="Tracking number"
+          className="w-full appearance-none rounded-l-md p-3 text-sm leading-tight text-gray-700 placeholder:text-sm focus:outline-none"
+        />
+
+        <button
+          className="inline-flex items-center rounded-r-md bg-indigo-200 py-2 px-4 transition-all hover:brightness-95 sm:text-sm"
+          onClick={onClick}
+        >
+          <Icon name="magnifyingGlass" className="h-5 w-5 text-indigo-500" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;
