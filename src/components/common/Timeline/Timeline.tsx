6be5a737852cada823b15f852841cdbe9c28b812
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-16 23:44:56 +0100

    Format date and update colors

--- src/components/common/Timeline/Timeline.tsx
+++ src/components/common/Timeline/Timeline.tsx
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 type TimelineItemProps = {
   date: string;
   location: string;
@@ -19,18 +21,19 @@ type TimelineProps = {
 };
 
 const TimelineItem = ({ date, location, description }: TimelineItemProps) => {
+  const newDate = new Date(date);
   return (
     <li>
       <div className="flex-start flex items-center pt-3">
-        <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-indigo-300 dark:bg-neutral-500"></div>
-        <p className="dark:text-neutral-00 text-xs text-neutral-300">{date}</p>
+        <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-500"></div>
+        <p className="text-sm text-neutral-400">
+          {format(newDate, "MMMM d, yyyy, h:mm aa")}
+        </p>
       </div>
 
       <div className="mt-2 ml-4">
         <h4 className="mb-1.5 text-sm font-semibold">{location}</h4>
-        <p className="pb-3 text-sm text-neutral-500 dark:text-neutral-300">
-          {description}
-        </p>
+        <p className="pb-3 text-sm text-neutral-400">{description}</p>
       </div>
     </li>
   );
