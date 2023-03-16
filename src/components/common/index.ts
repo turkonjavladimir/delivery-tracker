8bd39f762184bc98d32ee15d1aa83acda30be027
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-16 21:32:45 +0100

    Export SearchInput

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Avatar } from "./Avatar";
 export { default as Icon } from "./Icon";
 export { default as Card } from "./Card";
 export { default as Timeline } from "./Timeline";
+export { default as SearchInput } from "./SearchInput";
