dde88ee52c17dedecdd90da6b9b1e27506d661d6
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-21 20:12:28 +0100

    Create CardSkeleton

---
+++ src/components/common/Card/CardSkeleton.tsx
@@ -0,0 +1,14 @@
+export const CardSkeleton = () => {
+  return (
+    <div className="flex animate-pulse flex-col gap-3 rounded-md bg-white p-4 shadow-sm">
+      <div className="flex w-full justify-between">
+        <div className="flex flex-col justify-between gap-1">
+          <div className="h-3 w-48 rounded bg-slate-200"></div>
+          <div className="h-4 w-48 rounded bg-slate-200"></div>
+        </div>
+        <div className="h-[36px] w-16 rounded bg-slate-200"></div>
+      </div>
+      <div className="h-20 w-full rounded bg-slate-200"></div>
+    </div>
+  );
+};
