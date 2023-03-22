41b1e230678233fbcb4e5e44a256b295c00cf2f3
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-22 22:01:10 +0100

    Create Badge

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,6 +4,7 @@ export { default as Card } from "./Card";
 export { default as Timeline } from "./Timeline";
 export { default as SearchInput } from "./SearchInput";
 export { default as Button } from "./Button";
+export { default as Badge } from "./Badge";
 
 // Skeleton
 export { CardSkeleton } from "./Card/CardSkeleton";
