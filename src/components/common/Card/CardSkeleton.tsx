038d3dba761dd9029237beab55b652636164b18c
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-22 22:13:24 +0100

    Update CardSkeleton

--- src/components/common/Card/CardSkeleton.tsx
+++ src/components/common/Card/CardSkeleton.tsx
@@ -4,9 +4,11 @@ export const CardSkeleton = () => {
       <div>
         <div className="h-14 w-14 rounded bg-slate-200"></div>
       </div>
-      <div className="flex w-full flex-col justify-between">
-        <div className="h-3 w-full rounded bg-slate-200"></div>
-        <div className="h-3 w-full rounded bg-slate-200"></div>
+      <div className="flex w-full flex-col gap-2">
+        <div className="flex gap-3">
+          <div className="h-3 w-24 rounded bg-slate-200"></div>
+          <div className="h-3 w-16 rounded bg-slate-200"></div>
+        </div>
         <div className="h-3 w-full rounded bg-slate-200"></div>
       </div>
     </div>
