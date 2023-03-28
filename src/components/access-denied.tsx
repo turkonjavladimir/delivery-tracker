3594939c1bbfb6861b7d9e987b7dd90677224fb7
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-03-28 19:27:01 +0200

    aFix build error by replacing a tag with a button

--- src/components/access-denied.tsx
+++ src/components/access-denied.tsx
@@ -6,18 +6,19 @@ export default function AccessDenied() {
       <div>
         <h1 className="mb-5 text-3xl font-bold text-black">401</h1>
         <p className="text-2xl font-bold text-black">Access Denied</p>
+
         <p className="text-lg font-bold text-black">
-          <a
-            className="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
-            href="/api/auth/signin"
-            onClick={(e) => {
-              e.preventDefault();
-              signIn();
-            }}
-          >
-            You must be signed in to view this page
-          </a>
+          You must be signed in to view this page
         </p>
+        <button
+          className="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+          onClick={(e) => {
+            e.preventDefault();
+            signIn();
+          }}
+        >
+          Sign in
+        </button>
       </div>
     </div>
   );
